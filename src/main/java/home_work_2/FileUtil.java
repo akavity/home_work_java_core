4760f856441649a0a29b3d676ec76e36b795d357
akavity <basilakavity@gmail.com>
2023-03-04 02:52:46 +0300

    feat: add little change

--- src/main/java/home_work_2/FileUtil.java
+++ src/main/java/home_work_2/FileUtil.java
@@ -1,11 +1,7 @@
 package home_work_2;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Watchable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FileUtil {
 
@@ -41,23 +37,113 @@ public class FileUtil {
         return listOfLines;
     }
 
-    public List<String> returnListOfWordsStatingFromVowel(String source) throws IOException {
+    public List<String> returnListOfWordsStatingFromVowel(String source) {
         List<String> result = new ArrayList<>();
-        List<String> listOfWords;
-        String str = Files.readString(Path.of(source));
-        listOfWords = List.of(str.toLowerCase().split("(\\W+)"));
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] listOfWords = String.valueOf(stringBuilder).split("(\\W+)");
         for (String word : listOfWords) {
             char ch = word.charAt(0);
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'u' || ch == 'o' || ch == 'y') {
+                result.add(word);
+            }
+            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'U' || ch == 'O' || ch == 'Y') {
                 result.add(word);
             }
         }
         return result;
     }
 
+    public List<String> getListOfLines(String source) {
+        List<String> result = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] listOfWords = String.valueOf(stringBuilder).split("(\\W+)");
+        for (int i = 1; i < listOfWords.length; i++) {
+            if (listOfWords[i].charAt(0) == listOfWords[i - 1].charAt(listOfWords[i].length() - 1)) {
+                result.add(listOfWords[i - 1]);
+                result.add(listOfWords[i]);
+            }
+        }
+        return result;
+    }
+
+    public Map<Character, Integer> getLetterFrequency(String source) {
+        Map<Character, Integer> words = new HashMap<>();
+//        StringBuilder stringBuilder = new StringBuilder();
+        List<Character> symbols = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+            int symbol;
+            while ((symbol = bufferedReader.read()) != -1) {
+                symbols.add((char) symbol);
+            }
+
+            //  String[] letters = String.valueOf(stringBuilder).toLowerCase().split("");
+            //  int counter = 0;
+//            char symbol2 = 'a';
+//            while (symbol <= 'z') {
+//                for (String str : letters) {
+//                    if (symbol == str.charAt(0)) {
+//                        words.put(symbol, ++counter);
+//                    }
+//                }
+//                symbol++;
+//                counter = 0;
+//            }
+            int counter = 0;
+            for (char i = 'a'; i <= 'z'; i++) {
+                for (Character ch : symbols) {
+                    if (i == ch) {
+                        words.put(i, ++counter);
+                    }
+                }
+                counter = 0;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return words;
+    }
 
 
+    public void sortingAFile(String source) {
+        List<Integer> numbers = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String[] strings = stringBuilder.toString().split("(\\W+)");
+//        Arrays.sort(integers);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(source + "sort"))) {
+//            bufferedWriter.write(A);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
