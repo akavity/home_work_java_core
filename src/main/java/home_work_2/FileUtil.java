5ffc2afec922dc295cd85352d6d8111e72e5bc9a
akavity <basilakavity@gmail.com>
2023-03-03 15:57:59 +0300

    feat: add returnListOfLines method

--- src/main/java/home_work_2/FileUtil.java
+++ src/main/java/home_work_2/FileUtil.java
@@ -1,6 +1,8 @@
 package home_work_2;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtil {
 
@@ -16,12 +18,27 @@ public class FileUtil {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(target))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(target))) {
             bufferedWriter.write(String.valueOf(textInUpperCase));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
+
+    public List<String> returnListOfLines(String source) {
+        List<String> listOfLines = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                listOfLines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return listOfLines;
+    }
+
+
+
+
 }
