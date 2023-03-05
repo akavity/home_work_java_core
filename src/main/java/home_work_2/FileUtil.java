3054e1676ef940f9bb4d7334bb9d16650548464a
akavity <basilakavity@gmail.com>
2023-03-05 12:00:38 +0300

    feat: add readLines

--- src/main/java/home_work_2/FileUtil.java
+++ src/main/java/home_work_2/FileUtil.java
@@ -6,17 +6,7 @@ import java.util.*;
 public class FileUtil {
 
     public void copyFileInUpperCase(String source, String target) {
-        StringBuilder textInUpperCase = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                textInUpperCase.append(line).append("\n");
-            }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StringBuilder textInUpperCase = readLines(source, "\n");
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(target))) {
             bufferedWriter.write(String.valueOf(textInUpperCase).toUpperCase());
         } catch (IOException e) {
@@ -39,22 +29,12 @@ public class FileUtil {
 
     public List<String> returnListOfWordsStatingFromVowel(String source) {
         List<String> result = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StringBuilder stringBuilder = readLines(source, "\n");
         String[] listOfWords = String.valueOf(stringBuilder).split("(\\W+)");
         for (String word : listOfWords) {
             char ch = word.charAt(0);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'u' || ch == 'o' || ch == 'y') {
-                result.add(word);
-            }
-            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'U' || ch == 'O' || ch == 'Y') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'u' || ch == 'o' || ch == 'y'
+                    || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'U' || ch == 'O' || ch == 'Y') {
                 result.add(word);
             }
         }
@@ -63,15 +43,7 @@ public class FileUtil {
 
     public List<String> getListOfLines(String source) {
         List<String> result = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(" ");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StringBuilder stringBuilder = readLines(source, " ");
         String[] listOfWords = String.valueOf(stringBuilder).split("(\\W+)");
         for (int i = 1; i < listOfWords.length; i++) {
             if (listOfWords[i].charAt(0) == listOfWords[i - 1].charAt(listOfWords[i - 1].length() - 1)) {
@@ -81,17 +53,24 @@ public class FileUtil {
         return result;
     }
 
+//    public List<String> getListOfTheLargestCombinationOfNumbers(String source) {
+//        List<String> result = new ArrayList<>();
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+//            String line = null;
+//            while((line = bufferedReader.readLine()) != null) {
+//                String[] lineNumbers = line.split("(\\W+)");
+//
+//            }
+//        }
+//
+//
+//    }
+
     public Map<Character, Integer> getLetterFrequency(String source) {
         Map<Character, Integer> symbolsMap = new HashMap<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(" ");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StringBuilder stringBuilder = readLines(source, " ");
         char[] symbols = String.valueOf(stringBuilder).toLowerCase().toCharArray();
         int counter = 0;
         for (char i = 'a'; i <= 'z'; i++) {
@@ -107,15 +86,7 @@ public class FileUtil {
 
     public Map<String, Integer> sortTextByWords(String source) {
         Map<String, Integer> wordsMap = new HashMap<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(" ");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StringBuilder stringBuilder = readLines(source, " ");
         String[] words = String.valueOf(stringBuilder).split("(\\W+)");
         int count = 0;
         for (String word : words) {
@@ -130,15 +101,7 @@ public class FileUtil {
     }
 
     public void sortingAFile(String source) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(" ");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StringBuilder stringBuilder = readLines(source, " ");
         String[] strings = String.valueOf(stringBuilder).split("(\\W+)");
         int[] numbers = new int[strings.length];
         for (int i = 0; i < strings.length; i++) {
@@ -152,23 +115,37 @@ public class FileUtil {
         }
     }
 
-    public Map<String, Integer> getStudentsPerformance(String source) {
-        Map<String, Integer> studentPerformance = new TreeMap<>();
-        StringBuilder stringBuilder = new StringBuilder();
+    public Map<String, Double> getStudentsAverageMarks(String source) {
+        Map<String, Double> studentsAverageMarks = new HashMap<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line).append(" ");
+                String[] studentsAndMarks = line.split("(\\W+)");
+                double averageMark = 0.0;
+                for (int i = 1; i < studentsAndMarks.length; i++) {
+                    averageMark += Integer.parseInt(studentsAndMarks[i]);
+                }
+                averageMark /= (studentsAndMarks.length - 1);
+                studentsAverageMarks.put(studentsAndMarks[0], averageMark);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String[] studentsAndRating = String.valueOf(stringBuilder).split(",");
-        String[] student;
-        for (String str : studentsAndRating) {
-            student = str.trim().split(" ");
-            studentPerformance.put(student[0], Integer.parseInt(student[1]));
-        }
-        return studentPerformance;
+        return studentsAverageMarks;
     }
+
+    public StringBuilder readLines(String source, String regex) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append(regex);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return stringBuilder;
+    }
+
+
 }
