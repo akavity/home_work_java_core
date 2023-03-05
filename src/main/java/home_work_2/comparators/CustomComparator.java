88b49ca13de1420899c536a60c2af967cd9e743d
akavity <basilakavity@gmail.com>
2023-03-05 22:56:52 +0300

    feat: add CustomComparator

---
+++ src/main/java/home_work_2/comparators/CustomComparator.java
@@ -0,0 +1,22 @@
+package home_work_2.comparators;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public class CustomComparator implements Comparator<String> {
+    Map<String, Integer> map;
+
+    public CustomComparator(Map<String, Integer> map) {
+        this.map = map;
+    }
+
+    @Override
+    public int compare(String o1, String o2) {
+        if (map.get(o1) >= map.get(o2)) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+}
