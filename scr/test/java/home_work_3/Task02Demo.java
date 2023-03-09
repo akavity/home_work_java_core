0970a63722f0481728f0ea13938195c8cf16a51a
akavity <basilakavity@gmail.com>
2023-03-09 19:19:37 +0300

    feat: add Task02Demo

---
+++ scr/test/java/home_work_3/Task02Demo.java
@@ -0,0 +1,14 @@
+package home_work_3;
+
+
+public class Task02Demo {
+    public static void main(String[] args) {
+        Task02 test = new Task02();
+
+        System.out.println("Multiplication table one: ");
+        test.printTableInOneColumns();
+
+        System.out.println("Multiplication table two: ");
+        test.printTableInFiveColumns();
+    }
+}
