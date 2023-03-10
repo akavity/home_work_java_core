2a8a836dd0b16e8a28ae9341769d56bc129c67a6
akavity <basilakavity@gmail.com>
2023-03-10 22:10:24 +0300

    feat:  getFiveColumns

--- scr/test/java/home_work_3/Task02Demo.java
+++ scr/test/java/home_work_3/Task02Demo.java
@@ -1,15 +1,23 @@
 package home_work_3;
 
-
 public class Task02Demo {
     public static void main(String[] args) {
         Task02 test = new Task02();
+
         Columns columns = test::getColumns;
         columns.print(1, 1);
+
+        ColumnsFive columnsTwo = test::getFiveColumns;
+        columnsTwo.print();
     }
 
     @FunctionalInterface
     public interface Columns {
         void print(int a, int b);
+    }
+
+    @FunctionalInterface
+    public interface ColumnsFive {
+        void print();
     }
 }
