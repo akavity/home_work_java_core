bcb1c2d50bfcd38fc6240a890b0bf3a7a7aadf07
akavity <basilakavity@gmail.com>
2023-03-10 21:22:36 +0300

    fix: change solution

--- scr/main/java/home_work_3/Task02.java
+++ scr/main/java/home_work_3/Task02.java
@@ -1,31 +1,16 @@
 package home_work_3;
 
-import java.util.stream.IntStream;
-
 public class Task02 {
-
-    public void printTableInOneColumns() {
-        IntStream.rangeClosed(1, 10)
-                .forEach(i -> {
-                    IntStream.rangeClosed(1, 10)
-                            .forEach(j -> System.out.printf("%s * %s = %s\n", i, j, i * j));
-                    System.out.println();
-                });
-    }
-
-    public void printTableInFiveColumns() {
-        IntStream.rangeClosed(1, 10)
-                .forEach(i -> {
-                    IntStream.rangeClosed(1, 5)
-                            .forEach(j -> System.out.printf("%s * %s = %s\t", j, i, j * i));
-                    System.out.println();
-                });
-        System.out.println();
-        IntStream.rangeClosed(1, 10)
-                .forEach(i -> {
-                    IntStream.rangeClosed(6, 10)
-                            .forEach(j -> System.out.printf("%s * %s = %s\t", j, i, j * i));
-                    System.out.println();
-                });
+    public void getColumns(int multiplier, int multiplicand) {
+        while (multiplier <= 10) {
+            while (multiplicand <= 10) {
+                System.out.printf("%d * %d = %d       \t", multiplier, multiplicand, multiplier * multiplicand);
+                System.out.println(" ");
+                multiplicand++;
+            }
+            System.out.println("\n");
+            multiplicand = 1;
+            multiplier++;
+        }
     }
 }
--- scr/test/java/home_work_3/Task02Demo.java
+++ scr/test/java/home_work_3/Task02Demo.java
@@ -4,11 +4,12 @@ package home_work_3;
 public class Task02Demo {
     public static void main(String[] args) {
         Task02 test = new Task02();
+        Columns columns = test::getColumns;
+        columns.print(1, 1);
+    }
 
-        System.out.println("Multiplication table one: ");
-        test.printTableInOneColumns();
-
-        System.out.println("Multiplication table two: ");
-        test.printTableInFiveColumns();
+    @FunctionalInterface
+    public interface Columns {
+        void print(int a, int b);
     }
 }
