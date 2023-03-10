c65ed3025c9e1d0fb432345239a93ee3b3e79dd1
akavity <basilakavity@gmail.com>
2023-03-10 23:26:14 +0300

    fix: change solution

--- scr/main/java/home_work_3/Task01.java
+++ scr/main/java/home_work_3/Task01.java
@@ -4,33 +4,39 @@ import java.util.*;
 
 public class Task01 {
 
-    public static Map<String, String> getMapFromArray(List<String> strings) {
+    public Map<String, String> getMapFromArray(List<String> strings) {
         Map<String, String> map = new HashMap<>();
-        Runnable runnable = () -> {
-            String[] key = new String[1];
-            String[] value = new String[1];
-            for (String str : strings) {
-                if (str.charAt(0) == '-') {
-                    key[0] = str;
-                } else {
-                    value[0] = str;
-                    map.put(key[0], value[0]);
-                }
+        String[] key = new String[1];
+        String[] value = new String[1];
+        for (String str : strings) {
+            if (str.charAt(0) == '-') {
+                key[0] = str;
+            } else {
+                value[0] = str;
+                map.put(key[0], value[0]);
             }
-        };
-        runnable.run();
+        }
         return map;
     }
 
-    public static List<String> getArrayFromMap(Map<String, String> map) {
+    public List<String> getArrayFromMap(Map<String, String> map) {
         List<String> strings = new LinkedList<>();
-        Runnable runnable = () -> {
-            for (Map.Entry<String, String> item : map.entrySet()) {
-                strings.add(item.getKey());
-                strings.add(item.getValue());
-            }
-        };
-        runnable.run();
+        for (Map.Entry<String, String> item : map.entrySet()) {
+            strings.add(item.getKey());
+            strings.add(item.getValue());
+        }
         return strings;
+    }
+
+    public void printMap(Map<String, String> map) {
+        for (Map.Entry<String, String> item : map.entrySet()) {
+            System.out.printf("Key: %s   Value: %s\n", item.getKey(), item.getValue());
+        }
+    }
+
+    public void printList(List<String> list) {
+        for (String st : list) {
+            System.out.print(st + " ");
+        }
     }
 }
