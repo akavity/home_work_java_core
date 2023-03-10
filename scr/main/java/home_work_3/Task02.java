13cfb193321a2b7a845680b64b19a070bcf0a603
akavity <basilakavity@gmail.com>
2023-03-10 22:10:19 +0300

    feat:  getFiveColumns

--- scr/main/java/home_work_3/Task02.java
+++ scr/main/java/home_work_3/Task02.java
@@ -13,4 +13,20 @@ public class Task02 {
             multiplier++;
         }
     }
+
+    public void getFiveColumns() {
+        for (int i = 1; i <= 10; i++) {
+            for(int j = 1; j <= 5; j++) {
+                System.out.printf("%d * %d = %d       \t", j, i, j * i);
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+        for (int i = 1; i <= 10; i++) {
+            for(int j = 6; j <= 10; j++) {
+                System.out.printf("%d * %d = %d       \t", j, i, j * i);
+            }
+            System.out.println(" ");
+        }
+    }
 }
