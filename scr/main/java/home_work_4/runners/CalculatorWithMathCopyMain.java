85b6db8876b1072db639ad61b1032b5f29525a68
akavity <basilakavity@gmail.com>
2023-03-11 21:26:29 +0300

    add CalculatorWithMathCopyMain

---
+++ scr/main/java/home_work_4/runners/CalculatorWithMathCopyMain.java
@@ -0,0 +1,15 @@
+package home_work_4.runners;
+
+import home_work_4.calcs.simple.CalculatorWithMathCopy;
+
+public class CalculatorWithMathCopyMain {
+    public static void main(String[] args) {
+        CalculatorWithMathCopy calculator = new CalculatorWithMathCopy();
+        double div = calculator.div(28.5, 5);
+        double pow = calculator.pow(div, 2);
+        double mul = calculator.mul(15, 7);
+        double plus = calculator.plus(mul, pow);
+        double result = calculator.plus(4.1, plus);
+        System.out.println(result);
+    }
+}
