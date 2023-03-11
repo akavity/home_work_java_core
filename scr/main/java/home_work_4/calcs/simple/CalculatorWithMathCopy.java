c7f6b3bc6a318cc0c84dd9e8b2d3c1af8aa947f5
akavity <basilakavity@gmail.com>
2023-03-11 21:23:34 +0300

    add CalculatorWithMathCopy

---
+++ scr/main/java/home_work_4/calcs/simple/CalculatorWithMathCopy.java
@@ -0,0 +1,31 @@
+package home_work_4.calcs.simple;
+
+public class CalculatorWithMathCopy {
+    public double plus(double a, double b) {
+        return a + b;
+    }
+
+    public double minus(double a, double b) {
+        return a - b;
+    }
+
+    public double mul(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        return a / b;
+    }
+
+    public double pow(double a, int b) {
+        return Math.pow(a, b);
+    }
+
+    public double module(double a) {
+        return Math.abs(a);
+    }
+
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+}
