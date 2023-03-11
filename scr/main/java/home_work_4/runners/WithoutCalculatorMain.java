64d97543cc23eb28a9bab75d196ae82c275ae55d
akavity <basilakavity@gmail.com>
2023-03-11 20:09:54 +0300

    add WithoutCalculatorMain

---
+++ scr/main/java/home_work_4/runners/WithoutCalculatorMain.java
@@ -0,0 +1,8 @@
+package home_work_4.runners;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        double result = 4.1 + 15 * 7 + ((28.0 / 5)*(28.0 / 5));
+        System.out.println(result);
+    }
+}
