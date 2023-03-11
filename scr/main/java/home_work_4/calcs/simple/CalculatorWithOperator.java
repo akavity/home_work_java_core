23d04da803ec2ae7eb1121d4d215146dbd86e704
akavity <basilakavity@gmail.com>
2023-03-11 20:10:08 +0300

    add CalculatorWithOperator

---
+++ scr/main/java/home_work_4/calcs/simple/CalculatorWithOperator.java
@@ -0,0 +1,42 @@
+package home_work_4.calcs.simple;
+
+public class CalculatorWithOperator {
+
+    public double plus(double a, double b) {
+        return a + b;
+    }
+
+    public double minus(double a, double b) {
+        return a - b;
+    }
+
+    public double mul(double a, double b) {
+        return a * b;
+    }
+
+    public double div(double a, double b) {
+        return a / b;
+    }
+
+    public double pow(double a, int b) {
+        double result = 1;
+        for (int i = 0; i < b; i++) {
+            result = mul(result, a);
+        }
+        return result;
+    }
+
+    public double module(double a) {
+        return (a < 0.0) ? 0.0 - a : a;
+    }
+
+    public double squareRoot(double a) {
+        double b;
+        double result = a / 2;
+        do {
+            b = result;
+            result = (b + (a / b)) / 2;
+        } while ((b - result) != 0);
+        return result;
+    }
+}
