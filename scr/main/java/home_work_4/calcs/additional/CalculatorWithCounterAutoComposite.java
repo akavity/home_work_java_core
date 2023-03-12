39a1c57667634421bbd4add4b3bb856969e5600e
akavity <basilakavity@gmail.com>
2023-03-12 17:21:42 +0300

    remove blank lines

--- scr/main/java/home_work_4/calcs/additional/CalculatorWithCounterAutoComposite.java
+++ scr/main/java/home_work_4/calcs/additional/CalculatorWithCounterAutoComposite.java
@@ -20,36 +20,30 @@ public class CalculatorWithCounterAutoComposite {
         return calculator.pow(a, b);
     }
 
-
     public double module(double a) {
         incrementCountOperation();
         return calculator.module(a);
     }
-
 
     public double squareRoot(double a) {
         incrementCountOperation();
         return calculator.squareRoot(a);
     }
 
-
     public double plus(double a, double b) {
         incrementCountOperation();
         return calculator.plus(a, b);
     }
-
 
     public double minus(double a, double b) {
         incrementCountOperation();
         return calculator.minus(a, b);
     }
 
-
     public double mul(double a, double b) {
         incrementCountOperation();
         return calculator.mul(a, b);
     }
-
 
     public double div(double a, double b) {
         incrementCountOperation();
