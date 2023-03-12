85818278bd3643211b9d4e8e32b88289cbb86684
akavity <basilakavity@gmail.com>
2023-03-12 19:41:59 +0300

    feat: add ICalculator

---
+++ scr/main/java/home_work_4/calcs/api/ICalculator.java
@@ -0,0 +1,17 @@
+package home_work_4.calcs.api;
+
+public interface ICalculator {
+    double plus(double a, double b);
+
+    double minus(double a, double b);
+
+    double mul(double a, double b);
+
+    double div(double a, double b);
+
+    double pow(double a, int b);
+
+    double module(double a);
+
+    double squareRoot(double a);
+}
